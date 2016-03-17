166e3c0dac205fd98f4d4365a6f003ce94681c9d
fmarquesto <fmarquesto@bdev.com.ar>
2016-03-17 17:38:02 -0300

    second commit

---
+++ PrimerApp/src/clases/leerArchivo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Federico Marquesto
+ */
+public class leerArchivo {
+    
+    private String path;
+
+    public leerArchivo(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+    
+    
+    
+}
